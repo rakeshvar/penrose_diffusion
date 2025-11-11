Fixed penrose tiles to not have sign for side! Rotating canvas a bit also!!!

Generator.py
@@ -1,7 +1,5 @@
 import numpy as np
-from PIL import Image
 from abc import ABC, abstractmethod
-from typing import Optional
 
 from pen_base import PenGrid
 from hex_base import HexGrid
@@ -27,7 +25,7 @@ class Generator(ABC):
         self.canvas_xy = np.array([(h.x, h.y) for h in self.canvas], dtype=float)
         self.colors = np.array([h.color for h in self.canvas])
         self.angles = np.array([h.angle for h in self.canvas])
-        self.sides = np.array([h.side for h in self.canvas]) 
+        self.sides = np.array([h.side for h in self.canvas])
 
         self.imageset = imageset
         self.halfside = inscribed_square_halfside(self.canvas)
@@ -48,11 +46,11 @@ class Generator(ABC):
     @property
     def area_of_one_unit(self):
         return self.unit_area * self.unit_side ** 2
-        
+
     @property
     def density(self):
         return 1./self.area_of_one_unit
-    
+
     def get_sample(self):
         sample = next(self.imagesetiter)
         H, W = sample.mask.shape
@@ -62,39 +60,40 @@ class Generator(ABC):
         hw2c = lambda u: u * scaling
         eqsqhfsd = c2hw(np.sqrt(self.area_of_one_unit)) / 2.0  # Equivalent square half side
 
-        coverage = np.zeros(self.canvas_xy.shape[0], dtype=int)
-        def update_coverage(uu, vv):
-            is_in_bounds = (uu >= 0) & (uu < H) & (vv >= 0) & (vv < W)
-            coverage[is_in_bounds] += sample.mask[uu[is_in_bounds], vv[is_in_bounds]]            
 
-        # Rotate
+        # Rotate Canvas
         theta = np.random.uniform(-self.rot_range, self.rot_range)
-        rot_mat = np.array([
-            [np.cos(theta), np.sin(theta)],
-            [-np.sin(theta), np.cos(theta)]  # minus sin goes here because we are post multiplying @ rot_mat
-        ])
+        ct, st = np.cos(theta), np.sin(theta)
+        rot_mat = np.array([[ct, st], [-st, ct]])  # important minus goes here
         xy_rot = self.canvas_xy @ rot_mat
 
-        # Translate
+        # Translate Canvas
         x0 = np.random.uniform(-self.halfside, self.halfside - hw2c(H))
         y0 = np.random.uniform(-self.halfside, self.halfside - hw2c(W))
         new_xy = xy_rot - np.array([x0, y0])
 
+        # Rotate Mask
+        thetamask = np.random.uniform(-self.rot_range/3, self.rot_range/3)
+        ct, st = np.cos(thetamask), np.sin(thetamask)
+        rot_mask = np.array([[ct, -st], [st, ct]])
 
+        coverage = np.zeros(self.canvas_xy.shape[0], dtype=int)
+        def update_coverage(uu, vv):
+            uuvv = np.stack([uu, vv], axis=1) - np.array([H/2, W/2])
+            uuvv = uuvv @ rot_mask + np.array([H/2, W/2])
+            uu = np.round(uuvv[:, 0]).astype(int)
+            vv = np.round(uuvv[:, 1]).astype(int)
+            is_in_bounds = (uu >= 0) & (uu < H) & (vv >= 0) & (vv < W)
+            coverage[is_in_bounds] += sample.mask[uu[is_in_bounds], vv[is_in_bounds]]
+
+        # half-square corners in float coords
         uv = c2hw(new_xy)
-        u = uv[:, 0]
-        v = uv[:, 1]
+        u, v = uv[:, 0], uv[:, 1]
+        update_coverage(u - eqsqhfsd, v - eqsqhfsd)
+        update_coverage(u - eqsqhfsd, v + eqsqhfsd)
+        update_coverage(u + eqsqhfsd, v - eqsqhfsd)
+        update_coverage(u + eqsqhfsd, v + eqsqhfsd)
 
-        # compute corner indices
-        u1 = np.round(u - eqsqhfsd).astype(int)
-        v1 = np.round(v - eqsqhfsd).astype(int)
-        u2 = np.round(u + eqsqhfsd).astype(int)
-        v2 = np.round(v + eqsqhfsd).astype(int)
-        update_coverage(u1, v1)
-        update_coverage(u1, v2)
-        update_coverage(u2, v1)
-        update_coverage(u2, v2)
-    
         sets_idx = {val: np.flatnonzero(coverage == val) for val in (1, 2, 3, 4)}
         ret = np.zeros((self.sample_size, 5), dtype=float)
         taken = 0
@@ -127,15 +126,15 @@ class Generator(ABC):
 
 
 class Generator6(Generator):
-    unit_area = 3. * np.sqrt(3.) / 2.  
+    unit_area = 3. * np.sqrt(3.) / 2.
     rot_range = np.pi/6
 
     def _get_mother_tiles(self, tothalfside, unit_side):
         canvas = get_hex_mother_tiles(tothalfside, unit_side)
         hex_save_svg(canvas, "hex_canvas.svg")
         return canvas
-    
-    
+
+
 from pen_base import psi, psi2
 class Generator5(Generator):
     unit_area = np.sin(np.pi/5) * psi2 + np.sin(2*np.pi/5) * psi
@@ -145,10 +144,12 @@ class Generator5(Generator):
         canvas = get_pen_mother_tiles(tothalfside, unit_side)
         pen_save_svg(canvas, "pen_canvas.svg")
         return canvas
-    
+
 
 if __name__ == "__main__":
     from tqdm import tqdm
+    # tqdm = lambda x: x
+
     folder = "data/MPEG7"
     imageset = ImageSet(folder)
 
@@ -156,10 +157,10 @@ if __name__ == "__main__":
     for i in tqdm(range(len(imageset))):
         sample_matrix, name = generator6.get_sample()
         grid = HexGrid(sample_matrix)
-        hex_save_svg(grid, f"data/svgs_hex/{i:04d}_{name}.svg")
+        hex_save_svg(grid, f"data/svgs_hex/{name}.svg")
 
     generator5 = Generator5(imageset, sample_size=500, target_halfside=5., unit_side=.1)
     for i in tqdm(range(len(imageset))):
         sample_matrix, name = generator5.get_sample()
         grid = PenGrid(sample_matrix, from_np=True)
-        pen_save_svg(grid, f"data/svgs_pen/{i:04d}_{name}.svg")
+        pen_save_svg(grid, f"data/svgs_pen/{name}.svg")