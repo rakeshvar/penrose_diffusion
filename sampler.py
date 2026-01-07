fceb54e6159906f88daa58c25ae87ae39d2bd31e
rakeshvar <rakeshvar@gmail.com>
2026-01-07 22:36:55 +0530

    1) Removed data_config 2) tokens -> tiles 3) use_only removed 4) folders images, out, checkpoints, MPEG7 5) Removed argparse

---
+++ sampler.py
@@ -0,0 +1,75 @@
+import sys
+from pathlib import Path
+
+import torch
+import torch.nn.functional as F
+from ddim import DDIMDiffusion, TransformerDenoiser
+
+from hex_base import HexGrid
+from pen_base import PenGrid
+from hex_svg import save_svg as hex_save_svg
+from pen_svg import save_svg as pen_save_svg
+
+#--------------------------------------------
+# Argument Parsing
+#--------------------------------------------
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f"Using device: {device}")
+
+if len(sys.argv) < 2:
+    print(f"Usage: python {sys.argv[0]} checkpoint")
+    sys.exit()
+
+checkpoint_path = Path(sys.argv[1])
+assert checkpoint_path.exists(), f"Checkpoint {checkpoint_path} not found."
+
+checkpoint = torch.load(checkpoint_path, map_location=device)
+config = checkpoint['config']
+
+print("Config:")
+for k, v in config.items():
+    print(k)
+    for kk, vv in v.items():
+        print(f"\t{kk}: {vv}")
+
+model_config = config['model']
+train_config = config['train']
+
+#--------------------------------------------
+# Model Initialization
+#--------------------------------------------
+model = TransformerDenoiser(**model_config)
+model.to(device)
+
+diffuser = DDIMDiffusion(num_timesteps=1000) 
+model.load_state_dict(checkpoint['model_state_dict'])
+
+#--------------------------------------------
+# Sample
+#--------------------------------------------
+def sample(label, xyac, sample_fpath):
+    print("Generating sample...")
+    with torch.no_grad():
+        class_labels = torch.tensor([label], device=device)
+        samples = diffuser.sample(
+            model, batch_size=1, num_tiles=checkpoint['num_tiles'],
+            class_labels=class_labels, symmetry=checkpoint['symmetry'], num_steps=50
+        )
+
+        samples = samples.cpu().numpy()
+
+        if checkpoint['symmetry'] == 6:
+            grid = HexGrid(samples[0], side=checkpoint['side'])
+            hex_save_svg(grid, sample_fpath)
+        else:
+            grid = PenGrid(samples[0], from_np=True, side=checkpoint['side'])
+            pen_save_svg(grid, sample_fpath)
+
+    print(f"Saved sample -> {sample_fpath}")
+
+#--------------------------------------------
+# Main
+#--------------------------------------------
+while True:
+    sample(47, None, f"out/{checkpoint_path.stem}_ep{checkpoint['epoch']}.svg")
+    input("Press Enter to continue. Ctrl+C to exit...")
